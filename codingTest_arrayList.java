481518f0fc6a1932dedd0c1d03df8c9ea6f4de67
박지영 <barkji0@naver.com>
2021-05-24 23:38:46 +0900

    plant grass 1st

---
+++ codingTest_arrayList.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        for(int i=0; i<numbers.length; i++) {
+            for(int j=i+1; j<numbers.length; j++) {
+                int a = numbers[i] + numbers[j];
+                if (list.indexOf(a)<0) {
+                    list.add(a);
+                } 
+            }
+        }
+
+        int[] answer = new int[list.size()];
+
+        for(int i=0; i<list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+
+        Arrays.sort(answer);
+
+        return answer;
+    }
+}
